b8e9033a4e2a613289d91ce37bd325886197cf38
ERROR404040404 <87962047+ERROR404040404@users.noreply.github.com>
2025-09-10 13:42:58 -0700

    Basic Movement

---
+++ unity-solo-project_Hultquist/Assets/Scipts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    float inputX;
+    float inputY;
+
+    public float speed = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       Vector2 tempMove = rb.linearVelocity;
+
+        tempMove.x = inputX * speed;
+
+        rb.linearVelocity = (tempMove.x * transform.right) + (tempMove.y * transform.up);
+
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 InputAxis = context.ReadValue<Vector2>();
+
+        inputX = InputAxis.x;
+        inputY = InputAxis.y;
+    } 
+}

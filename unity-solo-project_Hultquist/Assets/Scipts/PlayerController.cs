15b7666ae75e93f6938b952fd3638197e57264f4
ERROR404040404 <87962047+ERROR404040404@users.noreply.github.com>
2025-09-24 11:10:04 -0700

    important stuff

--- unity-solo-project_Hultquist/Assets/Scipts/PlayerController.cs
+++ unity-solo-project_Hultquist/Assets/Scipts/PlayerController.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    Ray2D interactRay;
+    RaycastHit2D interactHit;
+    GameObject pickupObj;
+    Camera playerCam;
+
+    public PlayerInput input;
+    public Transform weaponSlot;
+    public Weapon currentWeapon;
+
 
     public Rigidbody2D rb;
 
@@ -19,31 +28,85 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public int health = 10;
     public int maxHealth = 10;
 
+    public float interactDistance = 1f;
+
     public float speed = 5f;
 
+    public bool attacking = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-   private void Start()
+    private void Start()
     {
+        input = GetComponent<PlayerInput>();
+        interactRay = new Ray2D(transform.position, transform.right);
+        weaponSlot = transform.GetChild(0);
+
+
         rb = GetComponent<Rigidbody2D>();
 
         //Cursor.visible = false; makes cursur non-visible
         // Cursor.lockState = CursorLockMode.Locked; locks cursor
-        
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-       Vector2 tempMove = rb.linearVelocity;
+        if (Physics2D.Raycast(transform.position, transform.right))
+        {
+            if (interactHit.collider.tag == "weapon")
+            {
+                pickupObj = interactHit.collider.gameObject;
+            }
+        }
+        else
+            pickupObj = null;
+
+        if (currentWeapon)
+            if (currentWeapon.holdToAttack && attacking)
+                currentWeapon.fire();
+
+
+        Vector2 tempMove = rb.linearVelocity;
 
         tempMove.x = inputX * speed;
 
-        if(health <= 0) //R U DED?
+        if (health <= 0) //R U DED?
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
+    public void Interact()
+    {
+        if (pickupObj)
+        {
+            if (pickupObj.tag == "weapon")
+                pickupObj.GetComponent<Weapon>().equip(this);
+        }
+    }
+    public void Reload()
+    {
+        if (currentWeapon)
+            currentWeapon.reload();
+    }
+        
 
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if (currentWeapon)
+        {
+            if (currentWeapon.holdToAttack)
+            {
+                if (context.ReadValueAsButton())
+                    attacking = true;
+                else attacking = false;
+            }
+            else if (context.ReadValueAsButton())
+                currentWeapon.fire();
+        }
+
+
+    }
     public void Move(InputAction.CallbackContext context)
     {
         Vector2 InputAxis = context.ReadValue<Vector2>();
